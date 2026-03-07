d3f50c86375192d3e46b279dd6346d19f4dfcd4e
Deepak S <sdeepak.shankar@gmail.com>
2026-03-07 17:30:16 +0530

    ideas module test cases

---
+++ web/modules/custom/ideas/tests/src/Unit/Controller/IdeasFileUploadControllerTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Drupal\Tests\ideas\Unit\Controller;
+
+use Drupal\ideas\Controller\IdeasFileUploadController;
+use Drupal\global_module\Service\FileUploadService;
+use Drupal\Tests\UnitTestCase;
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * @coversDefaultClass \Drupal\ideas\Controller\IdeasFileUploadController
+ * @group ideas
+ */
+class IdeasFileUploadControllerTest extends UnitTestCase {
+
+  protected $fileUploadService;
+  protected $controller;
+
+  protected function setUp(): void {
+    parent::setUp();
+
+    $this->fileUploadService = $this->createMock(FileUploadService::class);
+    $this->controller = new IdeasFileUploadController($this->fileUploadService);
+  }
+
+  /**
+   * @covers ::create
+   */
+  public function testCreate() {
+    $container = new ContainerBuilder();
+    $container->set('global_module.file_upload_service', $this->fileUploadService);
+    $controller = IdeasFileUploadController::create($container);
+    $this->assertInstanceOf(IdeasFileUploadController::class, $controller);
+  }
+
+  /**
+   * @covers ::upload
+   */
+  public function testUploadSuccess() {
+    $request = new Request();
+    $api_response = new JsonResponse(['fileName' => 'image.jpg']);
+    $this->fileUploadService->method('uploadFile')->with($request)->willReturn($api_response);
+
+    $response = $this->controller->upload($request);
+    $this->assertInstanceOf(JsonResponse::class, $response);
+    $this->assertEquals(json_encode(['fileUrl' => 'image.jpg']), $response->getContent());
+  }
+
+  /**
+   * @covers ::upload
+   */
+  public function testUploadError() {
+    $request = new Request();
+    $api_response = new JsonResponse(['error' => 'Invalid file'], 400);
+    $this->fileUploadService->method('uploadFile')->with($request)->willReturn($api_response);
+
+    $response = $this->controller->upload($request);
+    $this->assertInstanceOf(JsonResponse::class, $response);
+    $this->assertEquals(400, $response->getStatusCode());
+    $this->assertEquals(json_encode(['error' => 'Invalid file']), $response->getContent());
+  }
+
+}
